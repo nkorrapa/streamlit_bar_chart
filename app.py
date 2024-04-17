089ddcf970f8c9a3409b12fc747c87d2cd23e8f4
Neha Korrapati <neha.korrapati@gmail.com>
2024-04-17 11:43:33 -0400

    adding files

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+@st.cache_data
+def load_data(csv):
+    df=pd.read_csv(csv)
+    return df
+
+st.header("hello")
+
+pbp = load_data("data/2023pbp.csv")
+teams = load_data("data/teams.csv")
+
+st.dataframe(pbp)
+
+pbp = pbp.sort_values("event_player_1_name", ascending=True)
+players = pbp['event_player_1_name']
+players = players.drop_duplicates(ignore_index=True)
+
+player_name = st.selectbox("Pick a player", players.to_list())
+
+player_shots = pbp[(pbp.event_player_1_name == player_name) & (pbp.event_type == 'SHOT')]
+
+player_counts = player_shots.groupby('event_team_type', as_index=False).count()
+player_counts = pd.DataFrame(player_counts)
+player_counts = player_counts[['event_team_type','event']]
+
+fig, ax = plt.subplots()
+bar_colors = ['tab:red', 'tab:blue']
+ax.bar(player_counts.event_team_type, player_counts.event, color = bar_colors)
+ax.set_ylabel('Shots')
+ax.set_title(player_name)
+st.pyplot(fig= fig)
+
+
+# matplotlib
+# bar graph x = home/away, pick by player, y = shots
